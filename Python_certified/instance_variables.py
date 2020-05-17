1138e4df6e79152f504034e569bcf63c1cac08c9
srinivas dasu <srinivas.dasucse@gmail.com>
2020-05-17 23:44:17 +0530

    Adding python certification problems and topics

---
+++ Python_certified/instance_variables.py
@@ -0,0 +1,20 @@
+class ExampleClass:
+    def __init__(self, val = 1):
+        self.__first = val
+
+    def setSecond(self, val = 2):
+        self.__second = val
+
+
+exampleObject1 = ExampleClass()
+exampleObject2 = ExampleClass(2)
+
+exampleObject2.setSecond(3)
+
+exampleObject3 = ExampleClass(4)
+exampleObject3.__third = 5      # we can add variable to the object on fly like this   
+
+
+print(exampleObject1.__dict__)
+print(exampleObject2.__dict__)
+print(exampleObject3.__dict__)
